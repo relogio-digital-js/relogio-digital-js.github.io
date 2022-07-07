56b3ffeaae3f61020fe20d3ddf68b21c398573f8
relogio-digital-js <105955317+relogio-digital-js@users.noreply.github.com>
2022-07-07 14:05:41 -0300

    Add files via upload

---
+++ assets/data/data.js
@@ -0,0 +1,13 @@
+var data = new Date();
+
+var dia = String(data.getDate()).padStart(2, '0');
+
+var mes = String(data.getMonth() + 1).padStart(2, '0');
+
+var ano = data.getFullYear();
+
+dataAtual = dia + '/' + mes + '/' + ano;
+
+console.log(dataAtual);
+
+document.getElementById('resultado').innerHTML = "Hoje é " + dataAtual
